98492b654d8ca90dbfa8b784ad12dddf68c5e458
juaneuler <juaneuler@hotmail.com>
2026-01-18 17:37:18 -0300

    feat: add Bio component with styling

---
+++ src/components/Bio/Bio.tsx
@@ -0,0 +1,19 @@
+import "./Bio.scss";
+
+const Bio = () => {
+  return (
+    <section className="bio">
+      <h2>SOBRE LA BANDA</h2>
+      <p>
+        Compuesta por los hermanos Ross y Rocky Lynch, la banda surge en 2018 en
+        Los Angeles, California, como un nuevo desafío después de la separación
+        de R5. El grupo cuenta con 4 álbumes de estudio: X (2019), Girlfriend
+        (2021), Summer Mixtape (2022) y Obsession (2025), además de un álbum en
+        vivo: The Driver Era Live at the Greek (2023) y varios singles y
+        remixes.
+      </p>
+    </section>
+  );
+};
+
+export default Bio;
