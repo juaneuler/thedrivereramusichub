a7d894c7fce60f86e28cdf3c3d972d50c1d46a97
juaneuler <juaneuler@hotmail.com>
2026-02-07 16:03:50 -0300

    Add PWA support with service worker and web app manifest
    - Created a web app manifest file (site.webmanifest) for the application, defining its name, icons, theme color, and display settings.
    - Added a service worker (sw.js) with basic install, activate, and fetch event listeners.
    - Included icon images for the web app manifest (192x192 and 512x512 PNGs).
    - Registered the service worker in the main application file (main.tsx) to enable offline capabilities.

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,19 @@ import "./styles/main.scss";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch((err) => {
+      console.error("Error al registrar SW:", err);
+    });
+  });
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
